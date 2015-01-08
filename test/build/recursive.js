2ac9395692af9a5b32983c71a90a0ae0211e3b5d
Javey <jiawei23716@sina.com>
2015-01-08 10:50:41 +0800

    replace recursively

---
+++ test/build/recursive.js
@@ -0,0 +1,2 @@
+
+var b = "absolute_path";
---
+++ test/web/recursive.js
@@ -0,0 +1,2 @@
+/*document.write('<script type="text/javascript" src="/absolute_path.js"></script>');*/
+document.write('<script type="text/javascript" src="/absolute_path.js"></script>');
